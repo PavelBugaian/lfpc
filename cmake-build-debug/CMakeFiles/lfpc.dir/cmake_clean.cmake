75eec702aedbff25884f6f64f159003e8952830d
PavelBugaian <Ip558095>
2020-05-01 22:40:05 +0300

    Added 2nd lab and lexer

---
+++ cmake-build-debug/CMakeFiles/lfpc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lfpc.dir/lab2/main.c.o"
+  "lfpc"
+  "lfpc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lfpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
