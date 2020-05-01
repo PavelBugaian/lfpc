1b2cdab428f31eed288d681dcdf6c6bd91807354
PavelBugaian <Ip558095>
2020-05-01 11:52:19 +0300

    Added Lab2

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/bromgard/Projects/UTM/sem-1/lfpc/lab1
+# Install script for directory: /home/bromgard/Projects/UTM/sem-1/lfpc
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/bromgard/Projects/UTM/sem-1/lfpc/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/bromgard/Projects/UTM/sem-1/lfpc/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
